478e21f4b76a6bd357d80b73550697953b5ae683
EdcorC <33677744+EdcorC@users.noreply.github.com>
2017-11-14 22:48:31 -0500

    Add files via upload

---
+++ Pagos/boleta.designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class boleta
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtnom = New System.Windows.Forms.TextBox()
+        Me.txthoras = New System.Windows.Forms.TextBox()
+        Me.txtpagohora = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtsueldoneto = New System.Windows.Forms.TextBox()
+        Me.btnaceptar = New System.Windows.Forms.Button()
+        Me.btnlimpiar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre del Trabajador"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Horas trabajadas al mes"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(40, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Pago x hora S/."
+        '
+        'txtnom
+        '
+        Me.txtnom.Location = New System.Drawing.Point(133, 16)
+        Me.txtnom.Name = "txtnom"
+        Me.txtnom.Size = New System.Drawing.Size(187, 20)
+        Me.txtnom.TabIndex = 4
+        '
+        'txthoras
+        '
+        Me.txthoras.Location = New System.Drawing.Point(133, 41)
+        Me.txthoras.Name = "txthoras"
+        Me.txthoras.Size = New System.Drawing.Size(26, 20)
+        Me.txthoras.TabIndex = 5
+        '
+        'txtpagohora
+        '
+        Me.txtpagohora.Location = New System.Drawing.Point(133, 64)
+        Me.txtpagohora.Name = "txtpagohora"
+        Me.txtpagohora.Size = New System.Drawing.Size(67, 20)
+        Me.txtpagohora.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(45, 97)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Sueldo neto S/."
+        '
+        'txtsueldoneto
+        '
+        Me.txtsueldoneto.Enabled = False
+        Me.txtsueldoneto.Location = New System.Drawing.Point(133, 90)
+        Me.txtsueldoneto.Name = "txtsueldoneto"
+        Me.txtsueldoneto.Size = New System.Drawing.Size(100, 20)
+        Me.txtsueldoneto.TabIndex = 11
+        '
+        'btnaceptar
+        '
+        Me.btnaceptar.Location = New System.Drawing.Point(30, 133)
+        Me.btnaceptar.Name = "btnaceptar"
+        Me.btnaceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnaceptar.TabIndex = 12
+        Me.btnaceptar.Text = "Aceptar"
+        Me.btnaceptar.UseVisualStyleBackColor = True
+        '
+        'btnlimpiar
+        '
+        Me.btnlimpiar.Location = New System.Drawing.Point(133, 133)
+        Me.btnlimpiar.Name = "btnlimpiar"
+        Me.btnlimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnlimpiar.TabIndex = 13
+        Me.btnlimpiar.Text = "Limpiar"
+        Me.btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(235, 133)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnsalir.TabIndex = 14
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'boleta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 178)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btnlimpiar)
+        Me.Controls.Add(Me.btnaceptar)
+        Me.Controls.Add(Me.txtsueldoneto)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtpagohora)
+        Me.Controls.Add(Me.txthoras)
+        Me.Controls.Add(Me.txtnom)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "boleta"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Pago por Horas Trabajadas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtnom As System.Windows.Forms.TextBox
+    Friend WithEvents txthoras As System.Windows.Forms.TextBox
+    Friend WithEvents txtpagohora As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtsueldoneto As System.Windows.Forms.TextBox
+    Friend WithEvents btnaceptar As System.Windows.Forms.Button
+    Friend WithEvents btnlimpiar As System.Windows.Forms.Button
+    Friend WithEvents btnsalir As System.Windows.Forms.Button
+End Class
---
+++ Pagos/boleta.vb
@@ -0,0 +1,18 @@
+﻿Public Class boleta
+    Private Sub btnaceptar_Click(sender As System.Object, e As System.EventArgs) Handles btnaceptar.Click
+        Dim ht As Integer
+        'Se declara la variable para el ingreso de decimales
+        Dim ph As Decimal
+        ht = txthoras.Text
+        ph = txtpagohora.Text
+        txtsueldoneto.Text = ht * ph
+    End Sub
+
+    Private Sub btnsalir_Click(sender As System.Object, e As System.EventArgs) Handles btnsalir.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnlimpiar_Click(sender As System.Object, e As System.EventArgs) Handles btnlimpiar.Click
+
+    End Sub
+End Class
